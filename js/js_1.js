f28808fdd889f364adaed8fc40b0c52f8df36fd7
Nikola Stjepanović <nikola.stjepanovic@vsmti.hr>
2021-03-31 10:15:10 +0200

    Zad_4

--- js/js_1.js
+++ js/js_1.js
@@ -1,7 +1,10 @@
 function ispisTablice(){
-	document.getElementById("table").innerHTML+='<tr><td>'+ document.getElementById("np").value +'</td><td>'+ document.getElementById("gs").value + '</td><td>'+ document.getElementById("eb").value +'</td><td><img src="images/delete.png" onclick="deleteRow(this)"></td></tr>';
+	document.getElementById("table").innerHTML+='<tr><td>'+ document.getElementById("np").value +'</td><td>'+ document.getElementById("gs").value + '</td><td>'+ document.getElementById("eb").value +'</td><td><img src="images/delete.png" onclick="deleteRow(this)"></td><td><input type="checkbox" onclick="changeColor(this)" id="polozen"></td></tr>';
 }
 function deleteRow(r){
 	document.getElementById("table").deleteRow(r.parentNode.parentNode.rowIndex);
 
+}
+function changeColor(r){
+	document.getElementById("table").rows[r.parentNode.parentNode.rowIndex].style.backgroundColor = "blue";
 }
