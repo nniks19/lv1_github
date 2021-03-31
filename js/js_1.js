70baa8c40ad6ad03ae9a3c3eb234b9e252a6e2bb
Nikola Stjepanović <nikola.stjepanovic@vsmti.hr>
2021-03-31 09:14:50 +0200

    Zad_2

---
+++ js/js_1.js
@@ -0,0 +1,3 @@
+function ispisTablice(){
+		document.getElementById("table").innerHTML+='<tr><td>'+ document.getElementById("np").value +'</td><td>'+ document.getElementById("gs").value + '</td><td>'+ document.getElementById("eb").value +'</td></tr>';
+}
