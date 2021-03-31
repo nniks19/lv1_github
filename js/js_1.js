45f64345ccf4ad46462282f81431efb42608cf97
Nikola Stjepanović <nikola.stjepanovic@vsmti.hr>
2021-03-31 10:00:31 +0200

    Zad_3

--- js/js_1.js
+++ js/js_1.js
@@ -1,3 +1,7 @@
 function ispisTablice(){
-		document.getElementById("table").innerHTML+='<tr><td>'+ document.getElementById("np").value +'</td><td>'+ document.getElementById("gs").value + '</td><td>'+ document.getElementById("eb").value +'</td></tr>';
+	document.getElementById("table").innerHTML+='<tr><td>'+ document.getElementById("np").value +'</td><td>'+ document.getElementById("gs").value + '</td><td>'+ document.getElementById("eb").value +'</td><td><img src="images/delete.png" onclick="deleteRow(this)"></td></tr>';
+}
+function deleteRow(r){
+	document.getElementById("table").deleteRow(r.parentNode.parentNode.rowIndex);
+
 }
